6e393f236335dd11ee2ef74cecd65fc2b43d3e80
Iron Levyy <olegan.plav@gmail.com>
2025-06-01 14:09:42 +0300

    homework logging done

--- school/src/main/java/ru/hogvartz/school/Service/FacultyService.java
+++ school/src/main/java/ru/hogvartz/school/Service/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogvartz.school.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogvartz.school.Model.Faculty;
 import ru.hogvartz.school.Repository.FacultyRepository;
@@ -8,36 +10,42 @@ import java.util.Collection;
 
 @Service
 public class FacultyService {
-    final
-    FacultyRepository facultyRepository;
+    final FacultyRepository facultyRepository;
 
     public FacultyService(FacultyRepository facultyRepository) {
         this.facultyRepository = facultyRepository;
     }
 
+    private static final Logger logger = LoggerFactory.getLogger(FacultyService.class);
 
     public Faculty createFaculty(Faculty faculty) {
+        logger.info("Creating faculty: " + faculty);
         faculty.setId(null);
         return facultyRepository.save(faculty);
     }
 
     public Faculty getFaculty(Long id) {
+        logger.info("Getting faculty: " + id);
         return facultyRepository.findById(id).get();
     }
 
     public Faculty updateFaculty(Faculty faculty) {
+        logger.info("Updating faculty: " + faculty);
         return facultyRepository.save(faculty);
     }
 
     public void deleteFaculty(long id) {
+        logger.info("Deleting faculty: " + id);
         facultyRepository.deleteById(id);
     }
 
     public Collection<Faculty> getFacultyForColor(String color) {
+        logger.info("Getting faculty for color: " + color);
         return facultyRepository.findByColor(color);
     }
 
     public Collection<Faculty> getFacultyForColorOrName(String findTerm) {
+        logger.info("Getting faculty for term: " + findTerm);
         return facultyRepository.findByNameIgnoreCaseOrColorIgnoreCase(findTerm, findTerm);
     }
 }
--- school/src/main/java/ru/hogvartz/school/Service/StudentService.java
+++ school/src/main/java/ru/hogvartz/school/Service/StudentService.java
@@ -1,6 +1,7 @@
 package ru.hogvartz.school.Service;
 
-import org.springframework.data.domain.PageRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogvartz.school.Model.Student;
 import ru.hogvartz.school.Repository.StudentRepository;
@@ -13,8 +14,7 @@ import java.util.stream.Collectors;
 
 @Service
 public class StudentService {
-    final
-    StudentRepository studentRepository;
+    final StudentRepository studentRepository;
 
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
@@ -24,21 +24,26 @@ public class StudentService {
         student.setId(null);
         return studentRepository.save(student);
     }
+    private static final Logger logger = LoggerFactory.getLogger(StudentService.class);
 
     public StudentDTO getStudent(Long id) {
+        logger.info("Get student by id: " + id);
         Student student = studentRepository.getById(id);
         return StudentMapper.toDTO(student);
     }
 
     public Student updateStudent(Student student) {
+        logger.info("Update student by id: " + student.getId());
         return studentRepository.save(student);
     }
 
     public void deleteStudent(long id) {
+        logger.info("Delete student by id: " + id);
         studentRepository.deleteById(id);
     }
 
     public Collection<StudentDTO> getStudentsForAge(int age) {
+        logger.info("Get students for age " + age);
         List<Student> students = studentRepository.findByAge(age);
         return students.stream()
                 .map(StudentMapper::toDTO)
@@ -46,6 +51,7 @@ public class StudentService {
     }
 
     public Collection<StudentDTO> getStudentsBetweenAge(int min, int max) {
+        logger.info("Get students between age " + min + " and " + max);
         List<Student> students = studentRepository.findByAgeBetween(min, max);
         return students.stream()
                 .map(StudentMapper::toDTO)
@@ -53,14 +59,17 @@ public class StudentService {
     }
 
     public long getTotalStudents() {
+        logger.info("Get total students");
         return studentRepository.countAllStudents();
     }
 
     public Double getAverageAge() {
+        logger.info("Get average age");
         return studentRepository.findAverageAge();
     }
 
     public List<StudentDTO> getLastFiveStudents() {
+        logger.info("Get last five students");
         List<Student> students = studentRepository.findLastFiveStudents();
         return students.stream()
                 .map(StudentMapper::toDTO)
